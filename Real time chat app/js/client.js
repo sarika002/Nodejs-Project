f7442e19b7b9dee7834a5dfa7b7cb64a4d769107
sarika002 <98938893+sarika002@users.noreply.github.com>
2022-04-07 15:30:37 +0530

    Add files via upload

--- Real time chat app/js/client.js
+++ Real time chat app/js/client.js
@@ -1,1 +1,44 @@
+const socket = io('http://localhost:8000');
 
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector('.container');
+var audio = new Audio('messenger_tone.mp3');
+
+const append =(message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if(position == 'left'){
+        audio.play();
+    }
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value ='';
+
+})
+const Name = prompt('Enter your name to join');
+
+socket.emit('new-user-joined', Name);
+
+socket.on('user-joined', Name=>{
+    append(`${Name} joined the chat`, 'right')
+
+})
+
+socket.on('receive', data=>{
+    append(`${data.name} : ${data.message}`, 'left')
+
+})
+
+socket.on('left', name=>{
+    append(`${name} left the chat `, 'right')
+
+})
